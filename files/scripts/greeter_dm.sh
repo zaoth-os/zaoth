52e4aebf33066d8fe692a811d39a0b05403c3dc6
Geliardi D. Oliveira <91835201+fyllus@users.noreply.github.com>
2026-08-08 16:20:41 -0300

    Comment out lightdm service enable command
    Comment out the command to enable lightdm service.

--- files/scripts/greeter_dm.sh
+++ files/scripts/greeter_dm.sh
@@ -4,4 +4,4 @@ set -oeux pipefail
 systemctl disable gdm.service || true
 systemctl disable sddm.service || true
 systemctl disable greetd.service || true
-systemctl enable --force lightdm
+#systemctl enable --force lightdm
