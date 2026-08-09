e1728f1b95cf82b0c6ef8f577383698e6691f186
Fyllus <fylluszophia@proton.me>
2026-08-09 18:16:45 -0300

    chore: create new install system

--- files/scripts/zaoth_installs.sh
+++ files/scripts/zaoth_installs.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 set -oeux pipefail
 
-git clone https://github.com/zaoth-os/zaoth-share.git
-source zaoth-share/setup
-install-src
-
-git clone https://github.com/zaoth-os/zaoth-lib.git
-source zaoth-lib/setup
-install-src
+vermuz --install zaoth-os zaoth-share
+vermuz --install zaoth-os zaoth-lib
