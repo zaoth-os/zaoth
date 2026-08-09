47562f1f7c019431dccf7b18eb4271bb34a37dee
Fyllus <fylluszophia@proton.me>
2026-08-09 15:06:43 -0300

    Update zaoth_installs.sh

--- files/scripts/zaoth_installs.sh
+++ files/scripts/zaoth_installs.sh
@@ -3,12 +3,12 @@ set -oeux pipefail
 
 git clone https://github.com/zaoth-os/zaoth-share.git
 cd zaoth-share
-./install
+bash install
 cd ..
 rm -rf zaoth-share
 
 git clone https://github.com/zaoth-os/zaoth-lib.git
 cd zaoth-lib
-./install
+bash install
 cd ..
 rm -rf zaoth-lib
