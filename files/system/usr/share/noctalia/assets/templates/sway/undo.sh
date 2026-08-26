1e397b6ed620b2ad34b8d2a2a5b82f9a96a994c1
Fyllus <fylluszophia@proton.me>
2026-08-25 22:58:15 -0300

    chore: move noctalia share files to zaoth-share

--- files/system/usr/share/noctalia/assets/templates/sway/undo.sh
+++
@@ -1,7 +0,0 @@
-set -euo pipefail
-
-config_dir="${XDG_CONFIG_HOME:-$HOME/.config}/sway"
-config_file="$config_dir/theme/noctalia.color"
-font_file="$config_dir/theme/font"
-
-rm -f "$font_file" "$config_file"
